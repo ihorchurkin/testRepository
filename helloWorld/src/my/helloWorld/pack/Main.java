bcc06a08d76a582d4231f7960780e487afef507d
ihorchurkin <object.ihor@gmail.com>
2020-01-27 03:03:12 +0200

    exp format

--- helloWorld/src/my/helloWorld/pack/Main.java
+++ helloWorld/src/my/helloWorld/pack/Main.java
@@ -3,5 +3,7 @@ package my.helloWorld.pack;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		
+		System.out.printf("%.3e", Float.MAX_VALUE);
 	}
 }
